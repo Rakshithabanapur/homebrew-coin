0a4541a305d5e3ec3dc4b5131e86afeab9b0dc88
Rakshitha <rakshithabanapur29@gmail.com>
2025-12-07 23:20:10 +0530

    Update formula with SHA256 for v2.0

---
+++ coin.rb
@@ -0,0 +1,12 @@
+class Coin < Formula
+  desc "Command-line coin toss tool"
+  homepage "https://github.com/Rakshithabanapur/coin-app"
+  url "https://rakshithabanapur.github.io/coin-repo/coin_2.0.tar.gz"
+  version "2.0"
+  sha256 "454b6a1639bbeb40355483094aa0b6f09ce6676c9f8280b6d80f869031632cf9" 
+
+  def install
+    bin.install "coin"
+    man1.install "coin.1"
+  end
+end
